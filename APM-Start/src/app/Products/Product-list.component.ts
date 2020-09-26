07533435137507b5e02e22d6970b5f4900e4737b
kush.28011995 <kush.28011995@gmail.com>
2020-09-26 20:31:19 +0530

    Injecting the service using rootInjector

--- APM-Start/src/app/Products/Product-list.component.ts
+++ APM-Start/src/app/Products/Product-list.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {IProduct} from './product';
-
+import {ProductService} from './Product.service';
 @Component({
   // tslint:disable-next-line: component-selector
   selector: 'pm-Products',
@@ -9,19 +9,8 @@ import {IProduct} from './product';
 })
 export class ProductListComponent  implements OnInit {
 
-  pageTitle = 'Product List: No rating is clicked yet';
-  imageWidth = 50;
-  imageMargin = 2;
-  showImage = false;
-
-  filteredProducts: IProduct[];
+  constructor(private productService: ProductService){}
   _listFilter: string;
-
-  constructor(){
-    this.filteredProducts = this.products;
-    this._listFilter = 'cart';
-  }
-
   get listFilter(): string{
     return this._listFilter;
   }
@@ -29,6 +18,15 @@ export class ProductListComponent  implements OnInit {
     this._listFilter = value;
     this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter) : this.products;
   }
+
+  pageTitle = 'Product List: No rating is clicked yet';
+  imageWidth = 50;
+  imageMargin = 2;
+  showImage = false;
+
+  filteredProducts: IProduct[];
+
+  products: IProduct[];
 
   // tslint:disable-next-line: typedef
   performFilter(filterby: string){
@@ -41,64 +39,13 @@ export class ProductListComponent  implements OnInit {
     this.pageTitle = 'Product List: ' + rating;
   }
 
-  products: IProduct[] = [
-    {
-      "productId": 1,
-      "productName": "Leaf Rake",
-      "productCode": "GDN-0011",
-      "releaseDate": "March 19, 2019",
-      "description": "Leaf rake with 48-inch wooden handle.",
-      "price": 19.95,
-      "starRating": 3.2,
-      "imageUrl": "assets/images/leaf_rake.png"
-    },
-    {
-      "productId": 2,
-      "productName": "Garden Cart",
-      "productCode": "GDN-0023",
-      "releaseDate": "March 18, 2019",
-      "description": "15 gallon capacity rolling garden cart",
-      "price": 32.99,
-      "starRating": 4.2,
-      "imageUrl": "assets/images/garden_cart.png"
-    },
-    {
-      "productId": 5,
-      "productName": "Hammer",
-      "productCode": "TBX-0048",
-      "releaseDate": "May 21, 2019",
-      "description": "Curved claw steel hammer",
-      "price": 8.9,
-      "starRating": 4.8,
-      "imageUrl": "assets/images/hammer.png"
-    },
-    {
-      "productId": 8,
-      "productName": "Saw",
-      "productCode": "TBX-0022",
-      "releaseDate": "May 15, 2019",
-      "description": "15-inch steel blade hand saw",
-      "price": 11.55,
-      "starRating": 3.7,
-      "imageUrl": "assets/images/saw.png"
-    },
-    {
-      "productId": 10,
-      "productName": "Video Game Controller",
-      "productCode": "GMG-0042",
-      "releaseDate": "October 15, 2018",
-      "description": "Standard two-button video game controller",
-      "price": 35.95,
-      "starRating": 4.6,
-      "imageUrl": "assets/images/xbox-controller.png"
-    }
-  ];
-
   toggleImage(): void {
     this.showImage = !this.showImage;
   }
 
   ngOnInit(): void {
-    console.log('In OnInit');
+    this.products = this.productService.getProduct();
+    this.filteredProducts = this.products;
+    // because constructor will run before ngoninit thus removed from constructor
   }
 }
